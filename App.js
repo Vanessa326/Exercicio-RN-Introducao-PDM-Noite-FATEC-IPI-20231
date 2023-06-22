b1ab61ec48cbebcb88bb13670de8010250c60cd4
Vanessa Soares <vanis.sbs@gmail.com>
2023-06-22 12:56:49 -0300

    Apostila04(RN Intro): Exercício Intro React Native
    - Primeira versão do componente App garantindo a funcionalidade

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+
+const Separator = () => <View style={styles.separator} />;
 
 export default function App() {
+  
+  const [numeros, setNumeros] = useState(Array(6).fill(<i className="fas fa-money-bill-1-wave" ></i>));
+
+  const sorteio = () => {
+    setNumeros(Array.from({length: 6}, () => Math.floor(Math.random() * 60) + 1))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titleText}>Mega Sena</Text>
+      <Separator />
+      <View style={styles.middle}>
+        {numeros.map((valor) => <View  style={styles.item} ><Text>{valor}</Text></View>)}
+      </View>
+      <Button
+        title="Gerar Jogo"
+        onPress={sorteio}
+      />
+      
     </View>
   );
 }
@@ -14,7 +33,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    margin: 30,
+    borderColor: 'purple',
+    borderWidth: 2,
+    borderRadius: 20,
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'purple',
+    textAlign: 'left',
+  },
+  middle: {
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+    padding: 20,
+    margin: 10,
+    borderColor: 'purple',
+    flexDirection: 'row',
+    
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: 'purple',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  item: {
+    padding: 12,
+    backgroundColor: 'lightgreen',
+    borderColor: '#000',
+    borderWidth: 1,
+    marginBottom: 8,
+    borderRadius: 8,
   },
 });
